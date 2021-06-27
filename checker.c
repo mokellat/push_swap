f784d5d47cb047588739496adb40db934c91f228
Mohammed ali Kellati <mokellat@e0r3p6.1337.ma>
2021-06-27 21:01:32 +0100

    still some norme

--- checker.c
+++ checker.c
@@ -17,7 +17,7 @@ void	free_pointers(int status, char *line, int *stack_a, int *stack_b)
 	free(line);
 	free(stack_a);
 	free(stack_b);
-	exit(status);	
+	exit(status);
 }
 
 int exec(t_stack stack_a, t_stack stack_b, char *line)
@@ -49,54 +49,57 @@ int exec(t_stack stack_a, t_stack stack_b, char *line)
 	return (1);
 }
 
-void	check_int_double(char **argv, int argc)
+void	check_int_double(char **argv, int argc, int *stack_a)
 {
+	int	i;
+
+	i = 0;
 	if(!is_int(argv, argc) || !double_arg(argv, argc))
 	{
 		ft_putstr("ERROR\n");
 		exit(EXIT_FAILURE);
 	}
+	while (i < argc - 1)
+	{
+		stack_a[i] = ft_atoi(argv[i + 1]);
+		i++;
+	}
 }
 
-void	get_fun()
+void	sort(int *stack_a, int *stack_b, int size, char *line)
 {
-	
+	if (is_sorted(stack_a, &size))
+		ft_putstr("OK\n");
+	else
+		ft_putstr("KO\n");
+	free_pointers(EXIT_SUCCESS, line, stack_a, stack_b);
 }
 
 int main(int argc, char **argv)
 {
 	int		*stack_a;
 	int		*stack_b;
-	int		size;
-	int		size1;
+	int		size[2];
 	int		i;
 	char	*line;
 
 	i = 0;
-	size = argc - 1;
-	size1 = 0;
-	if (size < 1)
+	size[0] = argc - 1;
+	size[1] = 0;
+	if (size[0] < 1)
 		return (0);
-	check_int_double(argv, argc);
-	stack_a = malloc(sizeof(int) * size);
-	stack_b = malloc(sizeof(int) * size);
-	while (i < argc - 1)
-	{
-		stack_a[i] = ft_atoi(argv[i + 1]);
-		i++;
-	}
+	stack_a = malloc(sizeof(int) * size[0]);
+	check_int_double(argv, argc, stack_a);
+	stack_b = malloc(sizeof(int) * size[1]);
 	while(get_next_line(&line) > 0)
 	{
-		if (exec((t_stack){stack_a, &size},(t_stack){stack_b, &size1}, line) == 0)
+		if (exec((t_stack){stack_a, &size[0]},
+		(t_stack){stack_b, &size[1]}, line) == 0)
 		{
 			ft_putstr("Error\n");
 			free_pointers(EXIT_FAILURE, line, stack_a, stack_b);
 		}
 		free(line);
 	}
-	if (is_sorted(stack_a, &size))
-		ft_putstr("OK\n");
-	else
-		ft_putstr("KO\n");
-	free_pointers(EXIT_SUCCESS, line, stack_a, stack_b);
+	sort(stack_a, stack_b, size[0], line);
 }
